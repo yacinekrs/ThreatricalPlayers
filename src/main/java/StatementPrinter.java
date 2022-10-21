27585d09157685e5d6058b602e7d42e2a7a6aff4
KHORSI Yacine <yacinekhorsi1@gmail.com>
2022-10-21 14:00:54 +0200

    implementation StringBuilder

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -6,14 +6,13 @@ public class StatementPrinter {
   public String print(Invoice invoice, Map<String, Play> plays) {
     int totalAmount = 0;
     int volumeCredits = 0;
-    String result = String.format("Statement for %s\n", invoice.customer);
-
+    StringBuilder result = new StringBuilder(String.format("Statement for %s\n", invoice.customer));
     NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-
+    
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
       int thisAmount = 0;
-
+     
       switch (play.type) {
         case "tragedy":
           thisAmount = 40000;
@@ -35,15 +34,18 @@ public class StatementPrinter {
       // add volume credits
       volumeCredits += Math.max(perf.audience - 30, 0);
       // add extra credit for every ten comedy attendees
-      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+      if ("comedy".equals(play.type)) 
+        volumeCredits += Math.floor(perf.audience / 5);
 
       // print line for this order
-      result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
+      result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience));
+     
       totalAmount += thisAmount;
     }
-    result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
-    result += String.format("You earned %s credits\n", volumeCredits);
-    return result;
+    result.append(String.format("Amount owed is %s\n", frmt.format(totalAmount / 100)));
+    result.append(String.format("You earned %s credits\n", volumeCredits)) ;
+    String fullMessage =result.toString();
+    return fullMessage;
   }
 
 }
